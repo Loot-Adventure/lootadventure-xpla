df014a8703b1425b61a16a622375bf333f37f2cf
taijusanagi <taijusanagi@gmail.com>
2024-04-03 09:32:23 +0700

    done subgraph

--- 30_backend/02_SubGraph/subgraph/lootAdventure/src/mapping.ts
+++ 30_backend/02_SubGraph/subgraph/lootAdventure/src/mapping.ts
@@ -8,13 +8,32 @@ import {
 
 import { 
   Transfer as TransferRlootNftEvent,
-} from "../generated/RLootNft/SampleLootV2"
+} from "../generated/RLootNft/RLootNft"
+
+import { 
+  UpdateEquips as UpdateEquipsEvent,
+} from "../generated/SoulControler/SoulControler"
+
+import { 
+  updateEquipment as UpdateEquipmentEvent,
+} from "../generated/EquipmentNft/EquipmentNft"
+
+import { 
+  TransferSingle as TransferArtifactNftEvent,
+} from "../generated/ArtifactNft/ArtifactNft"
+
+import { 
+  TransferSingle as TransferJobNftEvent,
+} from "../generated/JobNft/JobNft"
 
 import {
   SoulLootNftAccount,
   SoulLootNft,
   User,
-  RLootNft
+  RLootNft,
+  Equipped,
+  Equipment,
+  ArtifactNftMinted
 } from "../generated/schema"
 
 export function handleAccountCreated(event: AccountCreatedEvent): void {
@@ -53,6 +72,11 @@ export function handleTransferSoulLootNft(event: TransferSoulLootNftEvent): void
 }
 
 export function handleTransferRLootNft(event: TransferRlootNftEvent): void {
+  let user = User.load(event.params.to.toHexString());
+  if(!user) {
+    user = new User(event.params.to.toHexString());
+    user.save();
+  }
   let rLootNft = RLootNft.load(
     event.params.tokenId.toString()
   )
@@ -65,3 +89,51 @@ export function handleTransferRLootNft(event: TransferRlootNftEvent): void {
   rLootNft.save();
 }
 
+export function handleUpdateEquips(event: UpdateEquipsEvent): void {
+  let equipped = new Equipped(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  equipped.weapon = event.params.equips.weapon.toString();
+  equipped.cheastArmor = event.params.equips.cheastArmor.toString();
+  equipped.headArmor = event.params.equips.headArmor.toString();
+  equipped.waistArmor = event.params.equips.waistArmor.toString();
+  equipped.footArmor = event.params.equips.footArmor.toString();
+  equipped.handArmor = event.params.equips.handArmor.toString();
+  equipped.necklace = event.params.equips.necklace.toString();
+  equipped.ring = event.params.equips.ring.toString();
+  equipped.save();
+  let soulLootNftAccount = SoulLootNftAccount.load(event.params.owner.toHexString());
+  if(!soulLootNftAccount) {
+    soulLootNftAccount = new SoulLootNftAccount(event.params.owner.toHexString());
+    soulLootNftAccount.save();
+  }
+  soulLootNftAccount.equipped = event.transaction.hash.concatI32(event.logIndex.toI32());
+  soulLootNftAccount.save();
+}
+
+export function handleUpdateEquipment(event: UpdateEquipmentEvent): void {
+  let equipment = Equipment.load(event.params._tokenId.toString());
+  if(!equipment) {
+    equipment = new Equipment(event.params._tokenId.toString());
+  }
+  equipment.level = event.params._level;
+  equipment.rarity = event.params._rarity;
+  equipment.save();
+}
+
+export function handleTransferArtifactNft(event: TransferArtifactNftEvent): void {
+  let artifactNftMinted = new ArtifactNftMinted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  artifactNftMinted.tokenId = event.params.id;
+  artifactNftMinted.owner = event.params.to.toHexString();
+  artifactNftMinted.save();
+}
+
+export function handleTransferJobNft(event: TransferJobNftEvent): void {
+  let soulLootNftAccount = SoulLootNftAccount.load(event.params.to.toHexString());
+  if(soulLootNftAccount) {
+    soulLootNftAccount.job = event.params.id;
+    soulLootNftAccount.save();
+  }
+}
